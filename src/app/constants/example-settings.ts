3a7bf03597805df73f8bf874433766dd6d4e8048
Mew-www <mew.proxy@hotmail.com>
2017-04-28 18:58:39 +0300

    Added boilerplate/example settings file (remove example- prefix and add production details for it to work with classes that use it)

---
+++ src/app/constants/example-settings.ts
@@ -0,0 +1,18 @@
+export class Settings {
+
+  public static API_BASE_URI = "<YOUR-PROXY-SERVER-API-ENDPOINT-HERE>";
+  public static REGIONS = [
+    {
+      id: "EUW",
+      full_name: "Europe West",
+      logo_uri: "<STATIC-FILE-URI>"
+    },
+    {
+      id: "EUNE",
+      full_name: "Europe Nordic & East",
+      logo_uri: "<STATIC-FILE-URI>"
+    }
+    // And any other whichever you like
+  ];
+
+}
